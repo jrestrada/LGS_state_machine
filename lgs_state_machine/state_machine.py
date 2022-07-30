92c60ea192eb4551ecfeffc38dfa3ad7eef01377
jrestrada <jramiroem@gmail.com>
2022-07-29 22:21:01 -0700

    separated functions for modularity

--- lgs_state_machine/state_machine.py
+++ lgs_state_machine/state_machine.py
@@ -11,37 +11,19 @@ from rclpy.action import ActionClient
 from std_msgs.msg import Int16
 from transitions.extensions import GraphMachine
 
-states = ['initializing',{'name':'top_elbow', 'on_enter':'crawl','on_exit':'hold'}, 'descent',
-         {'name':'bottom_elbow', 'on_enter':'crawl'},
-          'lateral_crawl','reached_end','retrieval','vertical_retrieval',
-          'finalizing','horizontal_failure','horizontal_failure',]
+states = ['initializing',{'name':'top_elbow', 'on_enter':'engage_safety','on_exit':'check_vertical'}, 
+         {'name':'vertical_pipe', 'on_enter':'advance'},
+         {'name':'bottom_elbow', 'on_enter':'engage_safety', 'on_exit':'check_horizontal'},
+         {'name':'lateral_crawl', 'on_enter':'advance'},'reached_end',
+         {'name':'retrieval', 'on_enter':'wind_up'},
+         {'name':'vertical_retrieval', 'on_enter':'wind_up'},
+          'finalized','horizontal_failure','horizontal_failure',]
 
 state_mach_params = dict(initial='initializing', title='WRPS Lateral Gamma Scanner State Machine', send_event= True,
-                  show_conditions=True, show_state_attributes=True)
+                  prepare_event = 'log_state' , show_conditions=True, show_state_attributes=True)
 
-def wait_for_input():
-    gotime = False
-    validselections =[0,1]
-    while gotime == False:
-            try:
-                print('select')
-                selection = int(input())
-                print('selection made')
-            except ValueError:
-                print("Invalid option! Select again")
-                continue
-            if selection in validselections:    
-                gotime == True
-                break
-            else:
-                print("Invalid option! Select again")
-    if selection == 0:
-        return False
-    if selection ==1:            
-        return True
-
-vertical_height = 60  # Inches
-lateral_length = 54   # Inches
+vertical_height = 10  # Inches
+lateral_length = 24   # Inches
 end_margin = 2        # Feet
 crawler_rate= 8       # inches per extension 
 # rc_synch = 19       # THIS SLEEP TIME MAINLY SYNCHRONIZES REEL AND CRAWLER
@@ -51,7 +33,6 @@ unwind_rate = 1.2     # reel vel to inches
 top_elbow_buffer = 2  # Extensions to clear top elbow after IMU vertical pitch 
 horizontal_angle = 15 # degrees
 vertical_angle = 80   # degrees
-
 
 crawler_commands = {
     "forward":       [1,3,5,2,4,6],
@@ -94,57 +75,31 @@ class StateMachineNode(Node):
         self.lidar_req = Checklidar.Request()
         self.lidar_req.requestlidar = 1
         time.sleep(2)
-
-    def is_vertical(self, cmd ='stop', times = 1):
-    def is_vertical(self, cmd ='stop', times = 1):
-        self.crawl(cmd)
-        self.get_logger().info("Checking if crawler has cleared top elbow")
-        # self.pitch_vertical = False
-        # while self.pitch_vertical == False:   
-        #     self.future = self._pitch_client.call_async(self.imu_req)
-        #     rclpy.spin_until_future_complete(self, self.future)
-        #     self.pitch = abs(self.future.result().pitch)
-        #     self.get_logger().info("Current inclination angle: {0}".format(self.pitch))
-        #     if self.pitch > 75 and self.pitch < 100:
-        #         self.pitch_vertical = True
-        #         self.get_logger().info("Top elbow cleared")
-        #         self.get_logger().info("Transitioning to state: {0}".format(self.state))
-        #         break
-        #     else:
-        #         self.get_logger().info("Not cleared top elbow yet, crawling forward")
-        #         self.crawl('forward_safe', 1)
-        leggo = False
-        while leggo == False:
-            wego = wait_for_input()
-            if wego == True:
-                self.get_logger().info("Top elbow cleared")
-                break
-            else:
-                self.crawl(cmd)
-        return True        
- 
-    def unwind_complete(self, dir = "unwind", times = 1):
-        self.elbow_is_reached = False
+   
+    def full_unwind(self, event):
+        self.unwind_complete = False
         self.unwound_length = 0.0
-        while self.elbow_is_reached == False:
+        while self.unwind_complete == False:
             if self.unwound_length >= vertical_height:
-                self.elbow_is_reached = True
+                self.unwind_complete = True
                 self.get_logger().info("Bottom elbow reached")
                 break
             else:
                 self.get_logger().info("Bottom elbow not reached")
                 self.activate_reel('unwind')
+                self.get_logger().info("Unwound : {0}".format(self.unwound_length))
                 self.unwound_length += reel_commands['unwind'][0]*wind_rate
                 time.sleep(reel_commands['unwind'][1]+0.1)
         return True  
 
-    def lateral_wind(self):
-        self.elbow_is_reached = False
+    def wind_up(self, event):
+        self.unwind_complete = False
         self.retrieved_length = 0.0
-        while self.elbow_is_reached == False:
-            if self.retrieved_length >= lateral_length:
-                self.elbow_is_reached = True
-                self.get_logger().info("Bottom elbow passed")
+        self.target_length = event.kwargs.get('len')
+        while self.unwind_complete == False:
+            if self.retrieved_length >= self.target_length:
+                self.unwind_complete = True
+                self.get_logger().info("retrieval section complete")
                 break
             else:
                 self.activate_reel('retrieve')
@@ -152,70 +107,62 @@ class StateMachineNode(Node):
                 self.get_logger().info("Retrieved : {0}".format(self.retrieved_length))
                 time.sleep(reel_commands['retrieve'][1]+0.1)
         self.activate_reel("stop")
-        return True  
     
+    def advance(self, event):
+        for i in range(event.kwargs.get('times')):
+            self.activate_crawler(event.kwargs.get('cmd'), 1 )
 
-    def current_pitch(self):
-        self.future = self._pitch_client.call_async(self.imu_req)
-        rclpy.spin_until_future_complete(self, self.future)
-        return abs(self.future.result().pitch)    
+    def get_current_pitch(self):
+        # self.future = self._pitch_client.call_async(self.imu_req)
+        # rclpy.spin_until_future_complete(self, self.future)
+        # return abs(self.future.result().pitch)    
+        print('type angle')
+        pitch_fake = input()
+        return int(pitch_fake)
+    
+    def get_current_distance(self):
+        # self.future = self._distance_client.call_async(self.lidar_req)
+        # rclpy.spin_until_future_complete(self, self.future)
+        # self.future = self._distance_client.call_async(self.lidar_req)
+        # rclpy.spin_until_future_complete(self, self.future)
+        # distance_fake = abs(self.future.result().distance)
+        print('type distance')
+        distance_fake = input()
+        return float(distance_fake)
 
     def is_hori(self):
-        self.pitch = self.current_pitch()
-        return (self.pitch < horizontal_angle & self.pitch < 100)
+        self.pitch = self.get_current_pitch()
+        return (self.pitch < horizontal_angle and self.pitch < 100)
 
-    def is_horizontal(self, cmd ='stop', times = 1):
-        self.get_logger().info("Checking if crawler has cleared bottom elbow")
-        self.pitch_horizontal = False
-        # while self.pitch_horizontal == False:   
-            # self.future = self._pitch_client.call_async(self.imu_req)
-            # rclpy.spin_until_future_complete(self, self.future)
-            # self.pitch = abs(self.future.result().pitch)
-            # self.get_logger().info("Current inclination angle: {0}".format(self.pitch))
-            # if self.pitch < 15 and self.pitch < 100:
-                # self.pitch_horizontal = True
-                # self.get_logger().info("Bottom elbow cleared")
-                # self.get_logger().info("Transitioning to state: {0}".format(self.state))
-                # time.sleep(2)
-                # break
-            # else:
-                # self.get_logger().info("Not cleared bottom elbow yet, crawling forward")
-                # self.crawl('forward_safe', 1)
-        leggo = False
-        while leggo == False:
-            wego = wait_for_input()
-            if wego == True:
-                self.get_logger().info("Bottom elbow cleared")
-                break
-            else:
-                self.crawl(cmd)
-        return True        
-        
-    def end_reached(self):
-        self.get_logger().info("Checking if crawler has reached end")
-        self.has_reached_end = False
-        # while self.has_reached_end == False:   
-        #     self.future = self._distance_client.call_async(self.lidar_req)
-        #     rclpy.spin_until_future_complete(self, self.future)
-        #     self.future = self._distance_client.call_async(self.lidar_req)
-        #     rclpy.spin_until_future_complete(self, self.future)
-        #     self.d_to_end = abs(self.future.result().distance)
-        #     self.get_logger().info("Distance until end: {0}".format(self.d_to_end))
-        #     if self.d_to_end < end_margin:
-        #         self.has_reached_end = True
-        #         break
-        #     else:
-        #         self.get_logger().info("Not reached end yet, crawling forward")
-        #         self.crawl('forward', 1)
-        leggo = False
-        while leggo == False:
-            wego = wait_for_input()
-            if wego == True:
-                self.get_logger().info("Bottom elbow cleared")
-                break
-            else:
-                self.crawl('forward', 1)
-        return True     
+    def is_vert(self):
+        self.pitch = self.get_current_pitch()
+        return (self.pitch > vertical_angle and self.pitch < 100)
+
+    def reached(self):
+        self.pitch = self.get_current_distance()
+        return (self.pitch < 2)
+
+    def check_vertical(self, event):
+        self.get_logger().info("Checking Verticality")
+        while not self.is_vert():
+            self.get_logger().info("Not vertical yet, crawling forward")
+            self.activate_crawler(event.kwargs.get('cmd'), event.kwargs.get('times'))
+        self.get_logger().info("Top elbow cleared")
+
+    def check_horizontal(self, event):
+        self.get_logger().info("Checking Horizontality")
+        while not self.is_hori():
+            self.get_logger().info("Not horizontal yet, crawling forward")
+            self.activate_crawler(cmd='forward_safe', times=1)
+        self.get_logger().info("Bottom elbow cleared")
+ 
+    def end_reached(self, event):
+        self.get_logger().info("Checking distance to end")
+        while not self.reached():
+            self.get_logger().info("Not reached end yet, crawling forward")
+            self.activate_crawler(cmd='forward', times=1)
+        self.get_logger().info("End reached")
+        return True
 
     def activate_reel(self, cmd="forward"):
         self.get_logger().info("Sending {0} reel request ".format(cmd))
@@ -225,40 +172,44 @@ class StateMachineNode(Node):
         reel_goal.reelcommand.continuous = reel_commands[cmd][2]
         self._reel_client.send_goal_async(reel_goal)
 
-    def crawl(self, dir='forward', times=1):
+    def activate_crawler(self, cmd = 'forward', times =1):
         self._crawler_client.wait_for_server(timeout_sec=2)
         self._reel_client.wait_for_server(timeout_sec=2)
         crawl_goal = Crawlaction.Goal()
-        crawl_goal.crawlercommand.crawlpattern = crawler_commands[dir]
+        crawl_goal.crawlercommand.crawlpattern = crawler_commands[cmd]
         crawl_goal.crawlercommand.continuous = False
         for i in range(times):
-            self.get_logger().info("Sending {0} crawl request ".format(dir))
-            self.activate_reel(dir)
+            self.get_logger().info("Sending {0} crawl request ".format(cmd))
+            self.activate_reel(cmd)
             self._crawler_client.send_goal_async(crawl_goal)
             time.sleep(rc_synch) 
             self.activate_reel("stop")
-            # if times > 1:
             self.hold()
     
+    def engage_safety(self, event):
+        self.activate_crawler("engage_back", 1)
+    
     def hold(self, cmd = "stop", times = 1):
-        self.get_logger().info("Pausing Crawl")
         time.sleep(2)
+
+    def log_state(self, event):
+        self.get_logger().info("Current state: {0}".format(self.state))
 
 def initialize_machine():
     rclpy.init()
     _sm = StateMachineNode()
     machine = GraphMachine(model=_sm, states=states ,**state_mach_params)
     machine.add_transition('crawl_in', source='initializing', dest='top_elbow')
-    machine.add_transition('check_angle', source='top_elbow', dest='descent', conditions='is_vertical', after ='crawl')
-    machine.add_transition('unwind', source='descent', dest='bottom_elbow', conditions='unwind_complete')
-    machine.add_transition('check_angle', source='bottom_elbow', dest='lateral_crawl', conditions='is_horizontal')
-    machine.add_transition('advance', source='lateral_crawl', dest='reached_end', conditions='end_reached')
+    machine.add_transition('descend', source='top_elbow', dest='vertical_pipe')
+    machine.add_transition('unwind', source='vertical_pipe', dest='bottom_elbow', conditions='full_unwind')
+    machine.add_transition('enter_lateral', source='bottom_elbow', dest='lateral_crawl')
+    machine.add_transition('check_end', source='lateral_crawl', dest='reached_end', conditions='end_reached')
     machine.add_transition('retrieve', source=['reached_end', 'horizontal_failure'], dest='retrieval')
     machine.add_transition('transition', source='retrieval', dest='vertical_retrieval')
     machine.add_transition('retrieve', source='vertical_failure', dest='vertical_retrieval')
     machine.add_transition('final_position', source='vertical_retrieval', dest='finalizing')
-    machine.add_transition('fail', source=['top_elbow','descent','bottom_elbow'], dest='vertical_failure')
-    machine.add_transition('fail', source=['lateral_crawl','reached_end' ], dest='horizontal_failure')
+    machine.add_transition('fail', source=['top_elbow','vertical_pipe'], dest='vertical_failure')
+    machine.add_transition('fail', source=['lateral_crawl','bottom_elbow' ], dest='horizontal_failure')
 
     machine.machine_attributes['ratio'] = '0.5'
        
@@ -267,32 +218,32 @@ def initialize_machine():
 
 def main(args=None):
     sm = initialize_machine()
-    sm.crawl("engage_back", 1)
+    sm.crawl_in(cmd = "forward_safe", times=1)
+    sm.descend(cmd = "forward_safe", times=top_elbow_buffer)
+    sm.activate_crawler(cmd = "release_back", times=1)
+    sm.unwind(cmd = "forward_safe", times=1)
+    sm.enter_lateral(cmd="forward", times=int(0.8*lateral_length/crawler_rate))
+    sm.check_end(cmd = "forward", times = 1)
+    sm.retrieve(len = lateral_length)
+    while True:
+        if sm.is_vert():
+            time.sleep(1)
+            break
+        else:
+            sm.activate_reel('retrieve')
+            time.sleep(2)
+            sm.activate_reel('stop')
+    sm.transition(len = vertical_height)
+    while True:
+        if sm.is_hori():
+            time.sleep(1)
+            break
+        else:
+            sm.activate_reel('retrieve')
+            time.sleep(2)
+            sm.activate_reel('stop')
+    sm.finalize()
     sm.get_logger().info("Current State: {0}".format(sm.state))
-    sm.crawl_in("forward_safe", 1)
-    time.sleep(1)
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-    sm.check_angle("forward_safe", top_elbow_buffer)
-    time.sleep(1)
-    sm.crawl("release_back", 1)
-    sm.unwind("engage_back", 1)
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-    time.sleep(1) 
-    sm.check_angle("forward_safe", 1)
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-    time.sleep(1)
-    sm.advance()
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-    time.sleep(1)
-    sm.retrieve()
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-    time.sleep(1)
-    sm.transition()
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-    time.sleep(1)
-    sm.final_position()
-    sm.get_logger().info("Current State: {0}".format(sm.state))
-
 
 if __name__ == "__main__":
     main()
